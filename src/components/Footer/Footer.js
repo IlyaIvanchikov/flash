c4dbe9fb2fd0631d0d26c06f132a55dc79fcd3ce
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-04-28 14:12:09 +0200

    feat: header adaptive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 const Footer = () => {
   return (
   <footer className="page-footer">
-    <div class="container">
+    <div className="container">
       &copy; 2020 Al & Il
     </div>
   </footer>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import Logo from '../../resources/img/SKILL.jpg';
+import Hidden from '@material-ui/core/Hidden';
+import Logo from '../../resources/img/tl.png';
 import './Header.css'
 
 const Header = () => {
   return (
-    <Grid container alignItems="center" className="header">
-      <Grid container item  xs={12} sm={6} direction="row" alignItems="center">
+    <header className="header">
+    <Grid container alignItems="center" >
+      <Grid container item  xs={8} sm={6} direction="row" alignItems="center">
         <img src={Logo} alt="logo" className="logo"/>
+        <Hidden smDown>
         <h1 className="h1-header">Флеш-карточки</h1>
+        </Hidden>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <p>Добро пожаловать!</p>
+      <Grid container justify="flex-end" item xs={4} sm={6}>
+        <p className="p-header">Добро пожаловать!</p>
       </Grid>
     </Grid>
+    </header>
   )
 }
 
