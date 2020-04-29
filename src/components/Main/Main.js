af16fcf18c59fb3d9b1104db5a9a387b477087a9
Alex G <archon.san@gmail.com>
2020-04-29 18:08:39 +0300

    feat: add result block

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import BlockPatameters from './BlockParameters/BlockParamaters'
-import Grid from '@material-ui/core/Grid'
+import BlockResult from './BlockResult/BlockResult'
+// import Grid from '@material-ui/core/Grid'
 import './Main.css'
 
 const Main = () => {
   const [time, setTime] = useState(0.1)
   const [countNumber, setCountNumber] = useState(1)
 
+
   return (
     <main className="main">
       <BlockPatameters />
+      <BlockResult test={[countNumber, time]} />
     </main>
   )
 }
