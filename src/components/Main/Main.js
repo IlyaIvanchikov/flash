1e7f1b868c2c2109366f0d55686f1e3379ecc43f
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-05-03 00:05:19 +0200

    feat: add header name

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,17 +4,17 @@ import BlockResult from './BlockResult/BlockResult'
 import BlockPlay from './BlockPlay/BlockPlay'
 import './Main.css'
 
-const Main = () => {
+const Main = (props) => {
   const [time, setTime] = useState(0.1)
   const [countNumber, setCountNumber] = useState(1)
   const [checkParams, setCheckParams] = useState(true)
-  const [name, setName] = useState('')
+  // const [name, setName] = useState('')
   const [checkResult, setCheckResult] = useState(false)
 
   const checkHandler = (time, countNumber, name) => {
     console.log(`time: ${time} + countNumber: ${countNumber} + name: ${name}`)
     setCountNumber(countNumber)
-    setName(name)
+    props.handleName(name)
     setTime(time)
     setCheckParams(false)
   }
@@ -29,7 +29,7 @@ const Main = () => {
   if (checkParams) {
     mainContent = <BlockParameters dataParams={checkHandler} />
   } else {
-    mainContent = <BlockResult time={time} name={name} stateHandler={resultHander}/>
+    mainContent = <BlockResult time={time} name={props.name} stateHandler={resultHander}/>
     // mainContent = <BlockPlay />
   }
   return <main className="main">{mainContent}</main>
