31375bb9d0b0bc14254d5d1dd94ef4b53dd85a37
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-05-04 00:27:53 +0200

    feat: problems with push asnwer

--- src/components/Main/BlockPlay/BlockPicture/BlockPicture.js
+++ src/components/Main/BlockPlay/BlockPicture/BlockPicture.js
@@ -1,38 +1,37 @@
 import React from 'react'
 import './BlockPicture.css'
-import Top0 from '../../../../resources/img/abakus/top0.png';
-import Top1 from '../../../../resources/img/abakus/top1.png';
-import Bottom0 from '../../../../resources/img/abakus/bottom0.png';
-import Bottom1 from '../../../../resources/img/abakus/bottom1.png';
-import Bottom2 from '../../../../resources/img/abakus/bottom2.png';
-import Bottom3 from '../../../../resources/img/abakus/bottom3.png';
-import Bottom4 from '../../../../resources/img/abakus/bottom4.png';
-
-
+import Top0 from '../../../../resources/img/abakus/top0.png'
+import Top1 from '../../../../resources/img/abakus/top1.png'
+import Bottom0 from '../../../../resources/img/abakus/bottom0.png'
+import Bottom1 from '../../../../resources/img/abakus/bottom1.png'
+import Bottom2 from '../../../../resources/img/abakus/bottom2.png'
+import Bottom3 from '../../../../resources/img/abakus/bottom3.png'
+import Bottom4 from '../../../../resources/img/abakus/bottom4.png'
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
-  
-  export default function MediaCard() {
-      const number = getRandomInRange(0, 9)
-      let bottomPicture
-      if (number === 0 || number === 5) bottomPicture = Bottom0
-      if (number === 1 || number === 6) bottomPicture = Bottom1
-      if (number === 2 || number === 7) bottomPicture = Bottom2
-      if (number === 3 || number === 8) bottomPicture = Bottom3
-      if (number === 4 || number === 9) bottomPicture = Bottom4
 
-    return (
-        <div className="blockPicture">
-            <div className="oneStick">
-                <div className="stickTop">
-                  <img alt="top" src={number > 4 ? Top1 : Top0} />
-                </div>
-                <div className="stickBottom">
-                  <img alt="bottom" src={bottomPicture} />
-                </div>
-            </div>
+export default function MediaCard(props) {
+  const number = getRandomInRange(0, 9)
+  props.currentHandler(number)
+  let bottomPicture
+  if (number === 0 || number === 5) bottomPicture = Bottom0
+  if (number === 1 || number === 6) bottomPicture = Bottom1
+  if (number === 2 || number === 7) bottomPicture = Bottom2
+  if (number === 3 || number === 8) bottomPicture = Bottom3
+  if (number === 4 || number === 9) bottomPicture = Bottom4
+
+  return (
+    <div className="blockPicture">
+      <div className="oneStick">
+        <div className="stickTop">
+          <img alt="top" src={number > 4 ? Top1 : Top0} />
+        </div>
+        <div className="stickBottom">
+          <img alt="bottom" src={bottomPicture} />
+        </div>
       </div>
+    </div>
   )
 }
--- src/components/Main/BlockPlay/BlockPlay.js
+++ src/components/Main/BlockPlay/BlockPlay.js
@@ -5,21 +5,20 @@ import Picture from './BlockPicture/BlockPicture'
 import './BlockPlay.css'
 
 const BlockPlay = (props) => {
-
   const [trueNumber, setTrueNumber] = useState([])
-  
+
   const paramsHandler = () => {
     props.stateHandler()
   }
-  let countTrueAnswer = [];
   const answerHandler = (number) => {
-    countTrueAnswer.push(number)
-    setTrueNumber(countTrueAnswer)
+    setTrueNumber(trueNumber => [...trueNumber, number])
+    // countTrueAnswer.push(number)
+    // setTrueNumber(countTrueAnswer)
   }
-  console.log(countTrueAnswer)
+  console.log(trueNumber)
   // console.log(trueNumber)
   const countPicture = [...Array(props.countNumber)].map((e, i) => (
-    <Picture key={i} currentHandler={answerHandler}/>
+    <Picture key={i} currentHandler={answerHandler} />
   ))
   return (
     <div className="blockPlay">
