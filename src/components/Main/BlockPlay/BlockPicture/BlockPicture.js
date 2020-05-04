d7ab95db2a7e1ae45bfb24311614882ef5a945e8
Alex G <archon.san@gmail.com>
2020-05-04 12:00:57 +0300

    fix: picture top0

--- src/components/Main/BlockPlay/BlockPicture/BlockPicture.js
+++ src/components/Main/BlockPlay/BlockPicture/BlockPicture.js
@@ -8,31 +8,29 @@ import Bottom2 from '../../../../resources/img/abakus/bottom2.png';
 import Bottom3 from '../../../../resources/img/abakus/bottom3.png';
 import Bottom4 from '../../../../resources/img/abakus/bottom4.png';
 
-
-
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
   
-  export default function MediaCard() {
-      const number = getRandomInRange(0, 9)
-      let bottomPicture
-      if (number === 0 || number === 5) bottomPicture = Bottom0
-      if (number === 1 || number === 6) bottomPicture = Bottom1
-      if (number === 2 || number === 7) bottomPicture = Bottom2
-      if (number === 3 || number === 8) bottomPicture = Bottom3
-      if (number === 4 || number === 9) bottomPicture = Bottom4
+export default function MediaCard() {
+    const number = getRandomInRange(0, 9)
+    let bottomPicture
+    if (number === 0 || number === 5) bottomPicture = Bottom0
+    if (number === 1 || number === 6) bottomPicture = Bottom1
+    if (number === 2 || number === 7) bottomPicture = Bottom2
+    if (number === 3 || number === 8) bottomPicture = Bottom3
+    if (number === 4 || number === 9) bottomPicture = Bottom4
 
-    return (
-        <div className="blockPicture">
-            <div className="oneStick">
-                <div className="stickTop">
-                  <img alt="top" src={number > 4 ? Top1 : Top0} />
-                </div>
-                <div className="stickBottom">
-                  <img alt="bottom" src={bottomPicture} />
-                </div>
-            </div>
+  return (
+    <div className="blockPicture">
+      <div className="oneStick">
+        <div className="stickTop">
+          <img alt="top" src={number > 4 ? Top1 : Top0} />
+        </div>
+        <div className="stickBottom">
+          <img alt="bottom" src={bottomPicture} />
+        </div>
       </div>
+    </div>
   )
 }
