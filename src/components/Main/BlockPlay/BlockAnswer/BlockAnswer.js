990cea7845b782809169dcf5e49d8b24a717ba1b
IlyaIvanchikov <phantomonster1@mail.ru>
2020-05-06 17:53:00 +0300

    feat: add comparison number

--- src/components/Main/BlockPlay/BlockAnswer/BlockAnswer.js
+++ src/components/Main/BlockPlay/BlockAnswer/BlockAnswer.js
@@ -1,71 +1,66 @@
 import React, { useState } from 'react'
 import { TextField, Button, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import SendIcon from '@material-ui/icons/Send';
+import SendIcon from '@material-ui/icons/Send'
 import './BlockAnswer.css'
-// import { doc } from 'prettier';
 
 const useStyles = makeStyles((theme) => ({
   button: {
     // margin: theme.spacing(1),
-    position: "absolute",
-    maxWidth: "40px",
-    transform: "translate(-118px, 11px)"
+    position: 'absolute',
+    maxWidth: '40px',
+    transform: 'translate(-118px, 11px)',
   },
   textField: {
     width: '30ch',
-    paddingRight: "20px"
+    paddingRight: '20px',
   },
 }))
 
-
 const BlockAnswer = (props) => {
-  
   const classes = useStyles()
   const [answerText, setAnswerText] = useState('')
 
-  const handleTextField = event => {
+  const handleTextField = (event) => {
     setAnswerText(event.target.value)
   }
 
-const startGame = () => {
+  const startGame = event => {
+    event.preventDefault();
     props.answerHandler(answerText)
-    setAnswerText('')
-}
+    document.querySelector('#outlined-secondary').value = ""
+  }
 
-return (
-  <div tabIndex="4" className="blockAnswer">
-    <Grid container>
-      <Grid item container justify="center" alignItems="center" xs={12}>
-      <form id="answerForm">
-          <TextField
-            type="number"
-            required
-            className={classes.textField}
-            id="outlined-secondary"
-            label="Введите ответ"
-            variant="outlined"
-            color="primary"
-            value={answerText}
-            onChange={handleTextField}
-          />
-          <Button
-            variant="contained"
-            id="submit"
-            // children={false}
-            type="submit"
-            color="primary"
-            prevent="true"
-            onClick={startGame}
-            className={classes.button}
-            startIcon={<SendIcon />}
-          >
-          </Button>
-        </form>
+  return (
+    <div tabIndex="4" className="blockAnswer">
+      <Grid container>
+        <Grid item container justify="center" alignItems="center" xs={12}>
+        <form id="answerForm" onSubmit={startGame}>
+            <TextField
+              type="number"
+              required
+              className={classes.textField}
+              id="outlined-secondary"
+              label="Введите ответ"
+              variant="outlined"
+              color="primary"
+              onChange={handleTextField}
+            />
+            <Button
+              variant="contained"
+              children={false}
+              id="submit"
+              type="submit"
+              color="primary"
+              prevent="true"
+              className={classes.button}
+              startIcon={<SendIcon />}
+            ></Button>
+          </form>
+        </Grid>
       </Grid>
-    </Grid>
-  </div>
-)
+    </div>
+  )
 }
 
 export default BlockAnswer
--- src/components/Main/BlockPlay/BlockPlay.js
+++ src/components/Main/BlockPlay/BlockPlay.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Control from './BlockControl/BlockControl'
 import Answer from './BlockAnswer/BlockAnswer'
 import Picture from './BlockPicture/BlockPicture'
@@ -6,6 +6,8 @@ import quastionMark from '../../../resources/img/quastion.jpg'
 import './BlockPlay.css'
 
 const BlockPlay = (props) => {
+  const correctAudioPlayer = useRef();
+  const wrongAudioPlayer = useRef();
   const countNumber = props.countNumber
   const countRound = props.countRound
   const time = props.time * 1000
@@ -25,16 +27,24 @@ const BlockPlay = (props) => {
   const [show, setShow] = useState(true)
   const [trueArr, setTrueArr] = useState(createArr(arr))
   const [specifiedAnswer, setSpecifiedAnswer] = useState('')
-
+  console.log(trueArr)
   const paramsHandler = () => {
     props.stateHandler()
   }
 
   const checkAnswer = (answer) => {
-    arr = []
     props.answerClickHandler()
-    setTrueArr(createArr(arr))
     setSpecifiedAnswer(answer)
+    const strAnswer = trueArr.join('')
+    console.log(strAnswer)
+    if (strAnswer === specifiedAnswer) {
+      correctAudioPlayer.current.play();
+    }
+    else {
+      wrongAudioPlayer.current.play();
+    }
+    arr = []
+    setTrueArr(createArr(arr))
     setShow(!show)
   }
 
@@ -55,8 +65,10 @@ const BlockPlay = (props) => {
       <div className="blockCard">
         {show && countPicture}
         {!show && thinkingPicture}
+        <audio src="https://zvukipro.com/uploads/files/2018-10/1540308965_jg-032316-sfx-elearning-incorrect-answer-sound-3.mp3" ref={correctAudioPlayer}/> 
+        <audio src="https://zvukipro.com/uploads/files/2018-10/1540308869_jg-032316-sfx-elearning-correct-answer-sound-1.mp3" ref={wrongAudioPlayer} />
       </div>
-      <Answer rounds={props.countRound} answerHandler={checkAnswer} />
+      <Answer rounds={props.countRound} answerHandler={checkAnswer} show={show}/>
     </div>
   )
 }
